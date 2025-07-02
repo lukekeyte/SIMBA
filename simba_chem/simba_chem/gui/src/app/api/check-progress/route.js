b9d7c69910fee52c1a023b2b0d7283181a86de92
Luke Keyte <45007726+lukekeyte@users.noreply.github.com>
2025-07-02 11:14:51 +0100

    Delete simba_chem directory

--- simba_chem/simba_chem/gui/src/app/api/check-progress/route.js
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  console.log('Progress check requested. Current progress:', global.solverProgress); // Debug log
-  return Response.json({ 
-    progress: global.solverProgress || 0 
-  });
-}
